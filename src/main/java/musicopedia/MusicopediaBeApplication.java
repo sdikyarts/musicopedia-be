4664bd27a129a1a781dc15832601f2d195a81f22
sdikyarts <wtrcrss.family@gmail.com>
2025-07-01 22:29:07 +0700

    Initial setups

---
+++ src/main/java/musicopedia/MusicopediaBeApplication.java
@@ -0,0 +1,13 @@
+package musicopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicopediaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicopediaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/musicopedia/MusicopediaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package musicopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicopediaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
