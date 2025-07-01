89a33f181c1a88b9a8d842552fd4d1157fe5dc14
sdikyarts <wtrcrss.family@gmail.com>
2025-07-01 23:25:46 +0700

    test: update test configurations

--- src/test/java/musicopedia/MusicopediaBeApplicationTests.java
+++ src/test/java/musicopedia/MusicopediaBeApplicationTests.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(TestConfig.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class MusicopediaBeApplicationTests {
 
     @Test
     void contextLoads() {
         // The test will pass if the application context loads successfully
     }
-
 }
--- src/test/java/musicopedia/config/TestConfig.java
+++ src/test/java/musicopedia/config/TestConfig.java
@@ -6,10 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @TestConfiguration
 @ActiveProfiles("test")
@@ -29,5 +34,34 @@ public class TestConfig {
         dataSource.setPassword("");
         
         return dataSource;
+    }
+    
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("musicopedia.model");
+        factory.setDataSource(dataSource());
+        
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.hbm2ddl.auto", "create-drop");
+        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        jpaProperties.put("hibernate.show_sql", "true");
+        jpaProperties.put("hibernate.format_sql", "true");
+        factory.setJpaProperties(jpaProperties);
+        
+        return factory;
+    }
+    
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return txManager;
     }
 }
