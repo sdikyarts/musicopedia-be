1f9f7add7a2d91a7d9495e1325197614199aeb86
sdikyarts <wtrcrss.family@gmail.com>
2025-07-01 23:23:55 +0700

    test: update test configurations

--- src/test/java/musicopedia/MusicopediaBeApplicationTests.java
+++ src/test/java/musicopedia/MusicopediaBeApplicationTests.java
@@ -1,15 +1,19 @@
 package musicopedia;
 
+import musicopedia.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestConfig.class)
 class MusicopediaBeApplicationTests {
 
     @Test
     void contextLoads() {
+        // The test will pass if the application context loads successfully
     }
 
 }
--- src/test/java/musicopedia/config/TestConfig.java
+++ src/test/java/musicopedia/config/TestConfig.java
@@ -1,10 +1,33 @@
 package musicopedia.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @TestConfiguration
 @ActiveProfiles("test")
+@EnableJpaRepositories(basePackages = "musicopedia.repository")
+@EntityScan(basePackages = "musicopedia.model")
+@EnableTransactionManagement
 public class TestConfig {
-    // Add test-specific beans if needed
+    
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        // Configure the test datasource
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        
+        return dataSource;
+    }
 }
