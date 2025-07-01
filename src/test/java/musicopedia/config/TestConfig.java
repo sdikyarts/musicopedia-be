d603efa9f1ce4211a3e4970e16030372bdf972a2
sdikyarts <wtrcrss.family@gmail.com>
2025-07-01 23:17:57 +0700

    test: create test configuration class

---
+++ src/test/java/musicopedia/config/TestConfig.java
@@ -0,0 +1,10 @@
+package musicopedia.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+
+@TestConfiguration
+@ActiveProfiles("test")
+public class TestConfig {
+    // Add test-specific beans if needed
+}
